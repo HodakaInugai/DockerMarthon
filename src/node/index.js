db7e4686d6b088d8ed960a8de32ee92d757b718d
Hodaka Inugai <hodaka.inugai@rakusu-partners.co.jp>
2023-12-15 09:55:05 +0900

    12KMまで

--- src/node/index.js
+++ src/node/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-
-const port = 自分の社員番号;
-
+const port = 3324;
 const cors = require("cors");
 app.use(cors());
-
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: "x", // PostgreSQLのユーザー名に置き換えてください
-  host: "localhost",
-  database: "x", // PostgreSQLのデータベース名に置き換えてください
-  password: "x", // PostgreSQLのパスワードに置き換えてください
+  user: "user_3324",
+  host: "172.18.0.2",
+  database: "crm_3324",
+  password: "pass_3324",
   port: 5432,
 });
 
@@ -20,7 +17,7 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/customers", async (req, res) => {
+app.get("/customer", async (req, res) => {
   try {
     const customerData = await pool.query("SELECT * FROM customers");
     res.send(customerData.rows);
@@ -30,14 +27,48 @@ app.get("/customers", async (req, res) => {
   }
 });
 
+app.get("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const customerData = await pool.query("SELECT * FROM customers WHERE customer_id = $1", [customerId]);
+
+    if (customerData.rows.length > 0) {
+      res.json({ success: true, customer: customerData.rows[0] });
+    } else {
+      res.json({ success: false, message: "Customer not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.json({ success: false, message: err.message });
+  }
+});
+
+app.delete("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const deleteResult = await pool.query("DELETE FROM customers WHERE customer_id = $1 RETURNING *", [customerId]);
+
+    if (deleteResult.rows.length > 0) {
+      res.json({ success: true, message: "Customer deleted successfully" });
+    } else {
+      res.json({ success: false, message: "Customer not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.post("/add-customer", async (req, res) => {
   try {
+    console.log(req.body);
+
     const { companyName, industry, contact, location } = req.body;
     const newCustomer = await pool.query(
-      "INSERT INTO customers (company_nam, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
+      "INSERT INTO customers (company_name, industry, contact, location) VALUES ($1, $2, $3, $4) RETURNING *",
       [companyName, industry, contact, location]
     );
     res.json({ success: true, customer: newCustomer.rows[0] });
@@ -47,4 +78,26 @@ app.post("/add-customer", async (req, res) => {
   }
 });
 
-app.use(express.static("public"));
+app.put("/customer/:customerId", async (req, res) => {
+  try {
+    const customerId = req.params.customerId;
+    const { companyName, industry, contact, location } = req.body;
+
+    const updatedCustomer = await pool.query(
+      "UPDATE customers SET company_name = $1, industry = $2, contact = $3, location = $4 WHERE customer_id = $5 RETURNING *",
+      [companyName, industry, contact, location, customerId]
+    );
+
+    if (updatedCustomer.rows.length > 0) {
+      // アップデート成功後、一覧画面に遷移
+      res.json({ success: true, customer: updatedCustomer.rows[0], redirectTo: 'list.html' });
+    } else {
+      res.json({ success: false, message: "Customer not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+});
+
+
